e8f5f445dd88d32875e02f78b9ec1d4797413063
Kamal <dragkamal71@gmail.com>
2024-02-23 15:31:51 +0530

    Initialize Project

--- src/components/RightPart.jsx
+++ src/components/RightPart.jsx
@@ -4,7 +4,7 @@ import { FaUser } from "react-icons/fa";
 const RightPart = () => {
   return (
     <>
-      <div className="w-4/5 bg-[#212d43]  h-full overflow-scroll">
+      <div className="w-4/5 bg-[#212d43]  h-full overflow-scroll pb-24">
         <div className=" grid grid-cols-3 gap-5 px-8 pt-5">
           <div className="shadow">
             <FaUser color="blue" />
@@ -41,6 +41,21 @@ const RightPart = () => {
             </div>
             <div className=" text-gray-500">DOJ : 20-10-2022</div>
           </div>
+          <div className="shadow">
+            <FaUser color="blue" />
+            <div className=" text-lg font-semibold ">
+              SAGAR MOLLA (SDFX562748)
+            </div>
+            <div className=" text-gray-500">DOJ : 20-10-2022</div>
+          </div>
+          <div className="shadow">
+            <FaUser color="blue" />
+            <div className=" text-lg font-semibold ">
+              SAGAR MOLLA (SDFX562748)
+            </div>
+            <div className=" text-gray-500">DOJ : 20-10-2022</div>
+          </div>
+         
         </div>
       </div>
     </>
