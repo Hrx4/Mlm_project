6bc14cfbe6e44db29998dfc13158775dce587516
Kamal <dragkamal71@gmail.com>
2024-04-07 15:44:10 +0530

    backend deployed

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,4 @@
-const backend = "http://localhost:8080"
+// const backend = "http://localhost:8080"
+const backend = "https://mlm-project-hh5m.vercel.app"
 
-export default backend
+export default backend;
